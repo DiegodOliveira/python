23e33f8af4cc39df8b41771ff970ce82ba97737e
DiegodOliveira <112182138+DiegodOliveira@users.noreply.github.com>
2022-09-14 09:31:04 -0300

    Add files via upload

---
+++ porção.py
@@ -0,0 +1,3 @@
+from math import trunc
+num = float(input("Digite um valor: "))
+print(f"O número que você digitou foi {num}, e a sua porção inteira foi {trunc(num)}")
---
+++ reajuste.py
@@ -0,0 +1,4 @@
+salario = float(input("Qual o seu salário?"))
+novo = salario + (salario * 15/100)
+
+print(f"O seu novo salário com reajuste será de R${novo:.2f}")
